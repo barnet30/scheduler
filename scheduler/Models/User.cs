90c50ed480d8d9b320067917dbabb2c10e7f8b84
Павел Гавриличев <PNGavrilichev@stud.kpfu.ru>
2021-04-29 03:31:37 +0300

    Добавьте файлы проекта.

---
+++ scheduler/Models/User.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace scheduler.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Password { get; set; }
+    }
+}
